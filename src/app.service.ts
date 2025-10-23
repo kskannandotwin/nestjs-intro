67d406de3d0ba564f4487413e7b71cb2376e291e
Jobulikannan <kannan@jobuli.com>
2025-10-23 06:17:52 +0530

    23.10.2025
    2-10-p completed

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { DummyService } from './dummy/dummy.service';
+import { LoggerService } from './logger/logger.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly dummyService: DummyService) { }
+  constructor(
+    private readonly dummyService: DummyService,
+    private readonly loggerService: LoggerService,
+  ) { }
   getHello(): string {
-    return `Hello World! ${this.dummyService.work()}`;
+    return this.loggerService.log(`Hello World! ${this.dummyService.work()}`);
   }
 }
---
+++ src/message-formatter/message-formatter.service.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MessageFormatterService {
+    format(message: string): string {
+        const timestamp = new Date().toISOString();
+        return `[${timestamp}] ${message}`;
+    }
+}
