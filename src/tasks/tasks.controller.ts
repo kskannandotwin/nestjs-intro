ac396ba15f06b6e3476d906bc24e4c33b978d123
Jobulikannan <kannan@jobuli.com>
2025-10-23 22:03:39 +0530

    23.10.2025
    3-17-yfe completed

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,10 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController { }
+export class TasksController {
+    @Get()
+    public findAll(): string[] {
+        return ['A', 'B'];
+    }
+}
