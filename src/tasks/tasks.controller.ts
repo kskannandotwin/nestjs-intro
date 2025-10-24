a0d1cef9e61f8d429bac21f08a2472db244bf3ed
Jobulikannan <kannan@jobuli.com>
2025-10-24 05:38:38 +0530

    24.10.2025
    3-18-wwddiu completed

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,18 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
+
     @Get()
     public findAll(): string[] {
         return ['A', 'B'];
     }
+
+    @Get('/:id')
+    public findOne(@Param('id') id: string): string {
+        return `The number is ${id}`;
+    }
+
+
 }
