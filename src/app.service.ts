db673bd590133db0e3a373b4363c3323f1cbbd22
Jobulikannan <kannan@jobuli.com>
2025-11-14 22:25:15 +0530

    14.11.2025
    4-39-ucsiys completed

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,19 @@
-/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { DummyService } from './dummy/dummy.service';
 import { LoggerService } from './logger/logger.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   constructor(
     private readonly dummyService: DummyService,
     private readonly loggerService: LoggerService,
-  ) { }
+    private readonly configService: ConfigService,
+  ) {}
   getHello(): string {
-    return this.loggerService.log(`Hello World! ${this.dummyService.work()}`);
+    const prefix = this.configService.get<string>('app.messagePrefix');
+    return this.loggerService.log(
+      `${prefix}Hello World! ${this.dummyService.work()}`,
+    );
   }
 }
