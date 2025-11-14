a53ee32ac711a2bd08de242c64d18b62502e0ea9
Jobulikannan <kannan@jobuli.com>
2025-11-14 22:05:00 +0530

    14.11.2025
    4-38-iasunc completed

---
+++ src/config/app.config.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import { registerAs } from '@nestjs/config';
+
+export const appConfig = registerAs('app', () => ({
+  messagePrefix: process.env.APP_MESSAGE_PREFIX ?? 'Hello ',
+}));
