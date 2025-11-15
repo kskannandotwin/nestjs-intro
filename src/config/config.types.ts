c27c049a2a90789eacd5c928ae17dc91745fc138
Jobulikannan <kannan@jobuli.com>
2025-11-15 09:58:23 +0530

    15.11.2025
    4-42-vcwj completed

--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -1,6 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { AppConfig } from './app.config';
+import * as Joi from 'joi';
 
 export interface ConfigType {
   app: AppConfig;
 }
+
+export const appConfigSchema = Joi.object({
+  APP_MESSAGE_PREFIX: Joi.string().default('Hello '),
+});
